bfdc7726e876432e3bda5c4d887d5599a18ba596
zizonyoungjun <naya700@naver.com>
2024-05-09 16:19:27 +0900

    Feat: 라우터 주소 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,20 +3,35 @@ import { createBrowserRouter } from "react-router-dom";
 import VoiceUpload from "../pages/voice/voiceUpload";
 import VoiceResult from "../pages/voice/voiceResult";
 import Loading from "../pages/voice/loading";
+import Survey from "../pages/survey/survey";
+import SurveyResult from "../pages/survey/surveyResult";
+import Landing from "../pages/landing";
 
 
 const Router = createBrowserRouter([
   {
     path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/voiceUpload",
     element: <VoiceUpload />,
   },
   {
-    path: "/VoiceResult",
+    path: "/voiceResult",
     element: <VoiceResult />,
   },
   {
-    path: "/Loading",
+    path: "/loading",
     element: <Loading />,
+  },
+  {
+    path: "/survey",
+    element: <Survey />,
+  },
+  {
+    path: "/surveyResult",
+    element: <SurveyResult />,
   },
 ]);
 
