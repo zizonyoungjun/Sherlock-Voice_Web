1ac753c02c096a50d3a9751c6f629d665c0c2ebf
zizonyoungjun <naya700@naver.com>
2024-05-09 16:18:53 +0900

    Design: icon,png 추가

--- public/assets/images/icons/stars.ts
+++
@@ -1,3 +0,0 @@
-export { ReactComponent as IcStarFull } from './IcStarFull.svg';
-export { ReactComponent as IcStarHalf } from './IcStarHalf.svg';
-export { ReactComponent as IcStarOff } from './IcStarOff.svg';
