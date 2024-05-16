af1bd0101188cbf42c0cc8d8960fc4c904c5714a
zizonyoungjun <naya700@naver.com>
2024-05-16 12:41:04 +0900

    Design: landing 애니메이션 수정

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -10,6 +10,24 @@ const PageContainer = styled.div`
   height: 100vh;
 `;
 
+const hoverAnimationTop = keyframes`
+  from {
+    background-color: #007BFF;
+  }
+  to {
+    background-color: #0056b3;
+  }
+`;
+
+const hoverAnimationBottom = keyframes`
+  from {
+    background-color: #28A745;
+  }
+  to {
+    background-color: #1e7e34;
+  }
+`;
+
 const Section = styled.div`
   flex: 1;
   display: flex;
@@ -18,32 +36,34 @@ const Section = styled.div`
   cursor: pointer;
   font-size: 24px;
   color: white;
-  background-color: #333;
-  &:hover {
-    background-color: #555;
-  }
 `;
 
 const TopSection = styled(Section)`
   background-color: #007BFF;
+  &:hover {
+    animation: ${hoverAnimationTop} 0.5s forwards;
+  }
 `;
 
 const BottomSection = styled(Section)`
   background-color: #28A745;
+  &:hover {
+    animation: ${hoverAnimationBottom} 0.5s forwards;
+  }
 `;
 
 const TopText = styled.p`
   text-align: center;
-  line-height: 1.5; // 라인 간격 조정
-  margin: 0 20px; // 좌우 마진 추가
+  line-height: 1.5;
+  margin: 0 20px;
   margin-top: 70px;
   font-size: 30px;
 `;
 
 const BottomText = styled.p`
   text-align: center;
-  line-height: 1.5; // 라인 간격 조정
-  margin: 0 20px; // 좌우 마진 추가
+  line-height: 1.5;
+  margin: 0 20px;
   margin-bottom: 100px;
   font-size: 30px;
 `;
