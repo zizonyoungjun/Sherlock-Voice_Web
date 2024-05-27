22a8f1207833122a222c70197227867f686d1bf1
zizonyoungjun <naya700@naver.com>
2024-05-27 13:07:52 +0900

    Design: voiceUpload margin-top  조정

--- src/pages/voice/voiceUpload.tsx
+++ src/pages/voice/voiceUpload.tsx
@@ -24,7 +24,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 100px;
+  margin-top: 120px;
   width: 90%; // 부모 요소에 맞춰 넓이 설정
 `;
 
